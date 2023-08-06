a6655978a631efd5950157228ca8d3304c72c148
Martyna "the other mk <67931479+TheOtherMK@users.noreply.github.com>
2023-08-06 14:39:52 +0100

    basic adding function working

--- script.js
+++ script.js
@@ -1,16 +1,44 @@
 let myLibrary = [];
 
-// function to put book together
-function Book() {
-  book_title = document.getElementById('title').value;
-  book_author = document.getElementById('author').value;
-  alert(book_title);
-  alert(book_author);
+//initialising a book
+function Book(title, author, pages, read) {
 
- // addBookToLibrary();
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+
 }
 
 function addBookToLibrary() {
-    myLibrary.push(this.bookvalue);
-    alert(myLibrary);
+
+  let title = document.querySelector('#title').value;
+  let author = document.querySelector('#author').value;
+  let pages = document.querySelector('#pages').value;
+  let read = document.querySelector('#read').checked; //as it's for a radio button
+
+  let newBook = new Book(title, author, pages, read);
+  console.log(newBook);
+
 }
+
+
+
+let newBookbtn = document.querySelector("#new-book-btn");
+
+newBookbtn.addEventListener("click", function() {
+  let newBookForm = document.querySelector("#add-book-form");
+  console.log(newBookForm);
+  newBookForm.style.display = "block";
+
+})
+
+document.querySelector("#add-book-form").addEventListener("submit", function () {
+  event.preventDefault(); //prevents default
+
+  addBookToLibrary(); //calls the function
+  
+  alert("Book Added!");
+
+
+})
